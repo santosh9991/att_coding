f7ce1b75cfa3c301f6d127182bb76411bf62a4a4
santosh9991 <santu1santosh@gmail.com>
2018-01-12 23:50:06 -0600

    requirements.txt added for deployment

--- att/att_repos/views.py
+++ att/att_repos/views.py
@@ -17,21 +17,25 @@ from decouple import config
 __author__ = "Santosh Kesireddy"
 __email__ = "santosh981k@gmail.com"
 class LoginRequiredMixin(object):
-    def __init__(self):
-        self.doc_string = \
-        "Login required class for class based views"
-    @classmethod
-    def as_view(cls):
-        return login_required(
-        	super(LoginRequiredMixin, cls).as_view())
-    def __doc__(self):
-    	return self.doc_string
-@login_required
-def home(request):
-	return JsonResponse("OAUTH2 worked",safe=False)
+	"""
+		Blocks unauthorised  users from accessing the application
+	"""
+	def __init__(self):
+		self.doc_string = "Login required class for class based views"
+	@classmethod
+	def as_view(cls):
+		return login_required(
+			super(LoginRequiredMixin, cls).as_view())
+	def __doc__(self):
+		return self.doc_string
+
 class AttIssueList(APIView):
+    """Returns att open issues from public repositories owned by 
+    Att organization. Each issue object also contains comments data
+    if exists for that issue 
+    Attributes:CLIENT_ID and CLIENT_SECRET: Required for large rate limit
+    """
     def __init__(self):
-        self.doc_string = " Att Public Repo open isues API"
         self.CLIENT_ID = config('CLIENT_ID')
         self.CLIENT_SECRET = config('CLIENT_SECRET')
     def authentication(self):
@@ -53,12 +57,12 @@ class AttIssueList(APIView):
            by the "att" organization. Each issue object also contain a 
            list of comments if any exists
         """
-
         try:
         	#request to get att public repositories
         	att_repos_obj = requests.get(
         	"https://api.github.com/orgs/att/repos?client_id="+self.CLIENT_ID+"&client_secret="+self.CLIENT_SECRET)
         except TypeError:
+        	#Exceptional handling for exceded rate limit
         	return JsonResponse("Rate Limit Exceded.")
         #Att Repos Data extraction from response object att_repos_obj 
         att_repos_dict_objs = json.loads(att_repos_obj.text)
@@ -67,11 +71,18 @@ class AttIssueList(APIView):
         for repo_name in repo_names:
             url = "https://api.github.com/repos/att/"+repo_name+"/issues?state=open&client_id="+self.CLIENT_ID+"&client_secret="+self.CLIENT_SECRET
             att_repo_response = requests.get(url)
-            att_repo_list_of_dict_objs = json.loads(att_repo_response.text)
-            att_open_issues_list.extend(att_repo_list_of_dict_objs)
-        att_open_issues_list = self.get_modified_list(att_open_issues_list)
+            #convert the request object to python native 
+            #data type object list
+            att_repo_list_of_dict_objs = json.loads(
+            	att_repo_response.text)
+            #extends the issues list for each iteration
+            att_open_issues_list.extend(
+            	att_repo_list_of_dict_objs)
+        att_open_issues_list = self.get_modified_list(
+        	att_open_issues_list)
         # return JsonResponse(att_open_issues_list,safe=False)
         return Response(att_open_issues_list)
-    def __doc__(self):
-    	return self.doc_string
+@login_required
+def home(request):
+	return JsonResponse("OAUTH2 worked",safe=False)
     
